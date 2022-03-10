f8828fbdc257a452b236aa0e460e36a9d92a332a
unknown <kenblackuoo@gmail.com>
2022-03-10 11:36:46 -0500

    Created the Song Model for the Songs App

--- musiclibrary_project/settings.py
+++ musiclibrary_project/settings.py
@@ -36,6 +36,8 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'corsheaders',
+    'songs',
+    'rest_framework',
 ]
 
 MIDDLEWARE = [
--- songs/models.py
+++ songs/models.py
@@ -1,3 +1,12 @@
+# (2.5 points) As a developer, I want to create a Song model. Property names must be in snake_case and match the following exactly!
+# title - CharField | artist - CharField | album - CharField | release_date - DateField | genre - CharField
+
 from django.db import models
 
-# Create your models here.
+
+class Song(models.Model):
+    title = models.CharField(max_length=255)
+    artist = models.CharField(max_length=255)
+    album = models.CharField(max_length=255)
+    release_date = models.DateField()
+    genre = models.CharField(max_length=50)
